21180508dbeb5c90ff91e626df2c953e8397fd91
THIENPHU_PC\phant <phanthienphu199@gmail.com>
2017-07-12 01:05:03 +0700

    add entitiy framework code first for data

---
+++ RoyalShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable,IDbFactory
+    {
+        RoyalShopDbContext dbContext;
+
+        public RoyalShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new RoyalShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ RoyalShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable //Interface có sẵn của C# cho phép các phương thức cài đặt từ nó có thể tự động huỷ
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+
+        // Ovveride this to dispose custom objects
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
