d3fefead6db1dfaaccf3f0703e66d2e9a27403ef
THIENPHU_PC\phant <phanthienphu199@gmail.com>
2017-07-12 00:04:03 +0700

    add domain entities for project

---
+++ RoyalShop.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoyalShop.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required] //bắt buộc nhập 
+        public string Content { set; get; }
+    }
+}
