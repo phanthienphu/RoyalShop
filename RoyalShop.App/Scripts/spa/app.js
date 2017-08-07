a4883b763579a859df57758a0d6350428ad3403d
THIENPHU_PC\phant <phanthienphu199@gmail.com>
2017-08-07 23:48:52 +0700

    What is scope and rootscope?

---
+++ RoyalShop.App/Scripts/spa/app.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("SchoolController", SchoolController);
+myApp.controller("StudentController", StudentController);
+myApp.controller("TeacherController", TeacherController);
+
+//myApp.$inject = ['$scope']; //hoạt động bình thường khi bỏ ở file ko phải dạng nén, khi nén file ở dạng .min sẽ bị lỗi
+//declare
+//function StudentController($rootScope,$scope) {
+//    $rootScope.message = "This is my message from Student";
+//};
+
+function SchoolController($Scope) {
+    $Scope.message = "Announcement from school.";
+};
+
+function StudentController($scope) {
+    $Scope.message = "This is my message from Student";
+};
+
+function TeacherController($scope) {
+   $scope.message = "This is my message from Teacher";
+}
