35165b653b3a70bbc44af76eeae31b1d732ea957
THIENPHU_PC\phant <phanthienphu199@gmail.com>
2017-08-15 23:32:19 +0700

    add filter status and format data in list

---
+++ RoyalShop.App/app/shared/filter/StatusFilter.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="/Common/Admin/libs/angular/angular.js" />
+
+(function (app) {
+    app.filter("StatusFilter", function () {
+        return function (input) {
+            if (input == true)
+                return "Kích hoạt";
+            else
+                return "Khoá";
+        }
+    });
+})(angular.module("royalshop.common"));
